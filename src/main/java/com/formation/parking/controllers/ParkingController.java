23adb258f59dc85047cf7036cab901b795a0a7f4
Youness <elkhatabi.youness@gmail.com>
2020-10-05 21:19:06 +0200

    create

---
+++ src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -0,0 +1,28 @@
+package com.formation.parking.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.parking.models.Parking;
+
+@RestController
+public class ParkingController {
+	
+	@RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
+	public List<Parking> getListeParkings(){
+		Parking parkingTest = new Parking();
+		parkingTest.setNom("Parking test");
+		parkingTest.setNbPlacesDispo(100);
+		parkingTest.setNbPlacesTotal(200);
+		parkingTest.setStatut("ouvert");
+		parkingTest.setHeureMaj("11h05");
+		ArrayList<Parking> liste = new ArrayList<Parking>();
+		liste.add(parkingTest);
+		return liste;
+	}
+
+}
