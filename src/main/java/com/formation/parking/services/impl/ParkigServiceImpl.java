2e5ca3fd785abbb73a1c5628988797cc2e82ffff
Youness <elkhatabi.youness@gmail.com>
2020-10-09 22:49:14 +0200

    add methode transformEntityToModel

--- src/main/java/com/formation/parking/services/impl/ParkigServiceImpl.java
+++ src/main/java/com/formation/parking/services/impl/ParkigServiceImpl.java
@@ -3,25 +3,42 @@ package com.formation.parking.services.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.formation.parking.dao.ParkingAPIDAO;
+import com.formation.parking.dao.entity.RecordEntity;
+import com.formation.parking.dao.entity.ReponseParkingAPIEntity;
 import com.formation.parking.models.Parking;
 import com.formation.parking.services.ParkingService;
 
 @Service
 public class ParkigServiceImpl implements ParkingService {
+	
+	@Autowired
+	public ParkingAPIDAO parkingAPIDAO;
 
 	@Override
 	public List<Parking> getListeParkings() {
-		Parking parkingTest = new Parking();
-		parkingTest.setNom("Parking test");
-		parkingTest.setNbPlacesDispo(100);
-		parkingTest.setNbPlacesTotal(200);
-		parkingTest.setStatut("FERME");
-		parkingTest.setHeureMaj("11h05");
-		ArrayList<Parking> liste = new ArrayList<Parking>();
-		liste.add(parkingTest);
-		return liste;
+		ReponseParkingAPIEntity reponse = parkingAPIDAO.getListeParking();
+		
+		return transformEntityToModel(reponse);
 	}
+
+	private List<Parking> transformEntityToModel(ReponseParkingAPIEntity reponse) {
+		List<Parking> resultat = new ArrayList<Parking>();
+		for (RecordEntity record : reponse.getRecords()) {
+			Parking parking = new Parking();
+			parking.setNom(record.getFields().getGrp_nom());
+			parking.setStatut(record.getFields().getGrp_statut());
+			parking.setNbPlacesDispo(record.getFields().getGrp_disponible());
+			parking.setNbPlacesTotal(record.getFields().getGrp_exploitation());
+			parking.setHeureMaj(record.getFields().getGrp_horodatage());
+			resultat.add(parking);
+		}
+		return resultat;
+	}
+	
+	
 
 }
