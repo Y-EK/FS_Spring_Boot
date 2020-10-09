61fa41b59dc359c03ecfc226e0cdfc9973d33831
Youness <elkhatabi.youness@gmail.com>
2020-10-09 22:42:01 +0200

    update

--- src/main/java/com/formation/parking/dao/ParkingAPIDAO.java
+++ src/main/java/com/formation/parking/dao/ParkingAPIDAO.java
@@ -1,5 +1,9 @@
 package com.formation.parking.dao;
 
+import com.formation.parking.dao.entity.ReponseParkingAPIEntity;
+
 public interface ParkingAPIDAO {
+	
+	public ReponseParkingAPIEntity getListeParking();
 
 }
