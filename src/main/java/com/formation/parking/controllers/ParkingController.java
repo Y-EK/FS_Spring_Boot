094514192787afaf796079e5b816877cc3541463
Youness <elkhatabi.youness@gmail.com>
2020-10-09 13:41:51 +0200

    add service interface and its implementation

--- src/main/java/com/formation/parking/controllers/ParkingController.java
+++ src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -1,28 +1,25 @@
 package com.formation.parking.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.parking.models.Parking;
+import com.formation.parking.services.ParkingService;
 
 @RestController
 public class ParkingController {
 	
+	@Autowired
+	private ParkingService parkingService;
+	
 	@RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
 	public List<Parking> getListeParkings(){
-		Parking parkingTest = new Parking();
-		parkingTest.setNom("Parking test");
-		parkingTest.setNbPlacesDispo(100);
-		parkingTest.setNbPlacesTotal(200);
-		parkingTest.setStatut("FERME");
-		parkingTest.setHeureMaj("11h05");
-		ArrayList<Parking> liste = new ArrayList<Parking>();
-		liste.add(parkingTest);
-		return liste;
+		
+		return parkingService.getListeParkings();
 	}
 
 }
